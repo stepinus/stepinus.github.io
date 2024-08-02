9a7b94b674c2f033f2f176735191a9d8065a825a
Yuriy Stepin <y.stepin@x5.ru>
2024-08-03 02:24:01 +0300

    frag

--- src/scenes/HyperBox/shaders/pointsFrag.glsl
+++ src/scenes/HyperBox/shaders/pointsFrag.glsl
@@ -3,9 +3,13 @@
 varying vec2 vUv;
 varying vec3 vPosition;
 varying vec3 vColor;
+uniform float boxOut;
+
 void main() {
   float uvLength = length(gl_PointCoord - 0.5);
   if (uvLength > 0.5 ) discard; // make'em round
+   float halfBoxOut = boxOut * 0.5;
+  if (abs(vPosition.x) > halfBoxOut || abs(vPosition.y) > halfBoxOut || abs(vPosition.z) > halfBoxOut) discard;
   if (abs(vPosition.x) > 20.) discard;
   vec3 col = mix(vColor, vColor * 0.875, ss(0.4, 0.5, uvLength)); // nice stroke
   gl_FragColor = vec4(col, 1);
