f6c6656c8c2e613a4a74b2bf9171679cd95671e2
Yuriy Stepin <y.stepin@x5.ru>
2024-08-02 18:22:51 +0300

    fix

--- old/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import glsl from "vite-plugin-glsl";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [glsl()],
-  // your other configuration...
-});
