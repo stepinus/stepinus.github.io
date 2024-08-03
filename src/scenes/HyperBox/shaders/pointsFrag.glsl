1d3f9875f7be20d1e2c89a1e56469dfeb0814a24
Yuriy Stepin <y.stepin@x5.ru>
2024-08-03 21:42:06 +0300

    fix

--- src/scenes/HyperBox/shaders/pointsFrag.glsl
+++ src/scenes/HyperBox/shaders/pointsFrag.glsl
@@ -4,11 +4,12 @@ varying vec2 vUv;
 varying vec3 vPosition;
 varying vec3 vColor;
 uniform float offset;
+uniform float particleOpacity;
 
 void main() {
   float uvLength = length(gl_PointCoord - 0.5);
   if (abs(vPosition.y) > offset || abs(vPosition.x) > offset || abs(vPosition.z) > offset) discard;
   // if (abs(vPosition.x) > 20.) discard;
   vec3 col = mix(vColor, vColor * 0.875, ss(0.4, 0.5, uvLength)); // nice stroke
-  gl_FragColor = vec4(col, particleOpacity);
+  gl_FragColor = vec4(col, 1);
 }
